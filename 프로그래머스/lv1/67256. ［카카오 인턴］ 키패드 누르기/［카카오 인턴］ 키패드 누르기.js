1e0002133346f634d92d9011aef5cf4700c7aabd
wonju <101818687+3o14@users.noreply.github.com>
2023-05-06 11:57:50 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.63 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
@@ -0,0 +1,50 @@
+function solution(numbers, hand) {
+    let answer = '';
+    const leftside = [1, 4, 7];
+    const rightside = [3, 6, 9];
+    const mid = [2, 5, 8, 0];
+    
+    let L_finger = "*"; 
+    let R_finger = "#";
+    
+    const pos = {
+        '1': [0, 0],
+        '2': [0, 1],
+        '3': [0, 2],
+        '4': [1, 0],
+        '5': [1, 1],
+        '6': [1, 2],
+        '7': [2, 0],
+        '8': [2, 1],
+        '9': [2, 2],
+        '*': [3, 0],
+        '0': [3, 1],
+        '#': [3, 2],
+    }
+    
+    const mid_distance = (L_finger, R_finger, target_num, hand) => {
+        let left_distance = Math.abs(pos[L_finger][0] - pos[target_num][0]) + Math.abs(pos[L_finger][1] - pos[target_num][1]);
+        let right_distance = Math.abs(pos[R_finger][0] - pos[target_num][0]) + Math.abs(pos[R_finger][1] - pos[target_num][1]);
+        
+       if(left_distance === right_distance) {
+           return hand === "right" ? "R" : "L";
+       } else {
+        return left_distance > right_distance ? "R" : "L";
+       }
+    }
+    
+    for(let i = 0; i < numbers.length; i++) {
+        // console.log(L_finger, R_finger, numbers[i]);
+        if(leftside.includes(numbers[i])){
+            answer += "L";
+            L_finger = numbers[i];
+        }else if(rightside.includes(numbers[i])){
+            answer += "R";
+            R_finger = numbers[i];
+        }else {
+            answer += mid_distance(L_finger, R_finger, numbers[i], hand);
+            answer.slice(-1) === "L" ? L_finger = numbers[i] : R_finger = numbers[i];
+        }
+    }
+    return answer;
+}
