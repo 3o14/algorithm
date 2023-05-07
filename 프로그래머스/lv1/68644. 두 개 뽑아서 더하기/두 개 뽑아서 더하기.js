5f4f6bee3ca44bfb2cece99dce98263bc437a0ee
wonju <101818687+3o14@users.noreply.github.com>
2023-05-07 13:03:10 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 3.22 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -9,10 +9,5 @@ function solution(numbers) {
         }
     }
     
-    answer.sort(function(a, b) {
-        if(a > b) return 1;
-        if(a === b) return 0;
-        if(a < b) return -1;
-    });
-    return answer;
+    return answer.sort((a, b) => a - b);
 }
