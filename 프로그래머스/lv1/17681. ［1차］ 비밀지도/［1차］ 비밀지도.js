9ff692b17cd46119794c1d90a9eb71ae1b5c4748
wonju <101818687+3o14@users.noreply.github.com>
2023-05-20 17:13:55 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,21 +1,12 @@
 function solution(n, arr1, arr2) {
     let answer = [];
+    let arr = [];
     
-    for(let i = 0; i < arr1.length; i++) {
-        arr1[i] = arr1[i].toString(2).padStart(n, 0).split("");
-        arr2[i] = arr2[i].toString(2).padStart(n, 0).split("");
+    for(let i = 0; i<n; i++){
+        arr.push((arr1[i] | arr2[i]).toString(2).padStart(n, 0));
     }
     
-    for(let i = 0; i < arr1.length; i++){
-        let arr = [];
-        for(let j = 0; j < arr1[i].length; j++){
-            if(arr1[i][j] === '1' || arr2[i][j] === '1'){
-                arr.push("#");
-            } else {
-                arr.push(" ");
-            }
-        }
-        answer[i] = arr.join("");
-    }
+    answer = arr.map(e => e.replace(/1|0/g, num => +num ? '#' : ' '));
+    
     return answer;
 }
