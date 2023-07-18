88d84162b3e43bf07374c508eafb34628395218d
wonju <101818687+3o14@users.noreply.github.com>
2023-07-18 23:50:52 +0900

    [Bronze V] Title: A+B, Time: 220 ms, Memory: 17784 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,11 +1,13 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
-		System.out.println(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		System.out.println(a+b);
 	}
 }
