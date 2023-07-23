db6ca6656d7aeb9296a691c01539d794951b6827
wonju <101818687+3o14@users.noreply.github.com>
2023-07-23 18:02:17 +0900

    [Bronze V] Title: 문자와 문자열, Time: 132 ms, Memory: 14460 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		String []str = st.nextToken().split("");
+		st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		for(int i=1; i<=str.length; i++) {
+			if(i == n) {
+				System.out.println(str[i-1]);
+			}
+		}
+	}
+}
