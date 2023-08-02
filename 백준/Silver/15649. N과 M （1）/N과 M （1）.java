4909f8675cfec48cdf1392ed95197e9cfdae65dd
wonju <101818687+3o14@users.noreply.github.com>
2023-08-02 09:15:46 +0900

    [Silver III] Title: N과 M (1), Time: 2144 ms, Memory: 60740 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,48 +1,57 @@
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	
-	static int N, R;
-	static int[] map;
-	static int[] nums;
-	static boolean[] v;
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		R = sc.nextInt();
-		map = new int[N];
-		nums = new int[R];
-		v = new boolean[N];
-		
-		for(int i=0; i<N; i++) {
-			map[i] = i+1;
-		}
-		// input end
-		
-		perm(0);
-	}
-	
-	public static void perm(int cnt) {
-		// 종료
-		if(cnt == R) {
-			// 시뮬레이션 구현
-			for(int i=0; i<R; i++) {
-				System.out.print(nums[i] + " ");
-			}
-			System.out.println();
-			return;
-		}
-	
-		// 실행 & 재귀 호출
-		for(int i=0; i<N; i++) {
-			if(v[i]) {
-				continue;
-			}
-			nums[cnt] = map[i];
-			v[i] = true;
-			perm(cnt + 1);
-			v[i] = false;
-		}
-	}
+
+    static boolean[] visited;
+    static int[] nums;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] line = br.readLine().split(" ");
+        int N = Integer.parseInt(line[0]);
+        int M = Integer.parseInt(line[1]);
+
+        nums = new int[N];
+        for(int i = 0; i<N; i++){
+            nums[i] = i+1;
+        }
+
+        visited = new boolean[N];
+        arr = new int[M];
+
+        combination(N, M, 0);
+
+
+    }
+
+    public static void combination(int N, int M, int index){
+        if(index == M){
+            //printNums(N);
+            for(int n:arr){
+                System.out.print(n + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i = 0; i<N; i++){
+            if(visited[i] == false) {
+                visited[i] = true;
+                arr[index] = i + 1;
+                combination(N, M, index + 1);
+                visited[i] = false;
+            }
+        }
+    }
+
+    private static void printNums(int N) {
+        for(int i = 0; i< N; i++){
+            if(visited[i]) System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+    }
 }
