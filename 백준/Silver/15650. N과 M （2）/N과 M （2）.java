9258884a980f80105f714e6871d18aa5e42036c3
wonju <101818687+3o14@users.noreply.github.com>
2023-08-02 13:31:09 +0900

    [Silver III] Title: N과 M (2), Time: 156 ms, Memory: 15832 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,42 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main{
-	static int N, M;
-	static boolean[] isChecked;
-	static int[] numbers;
+public class Main {
+    static int N, R;
+    static int[] input, sel;
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	
+        N = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        input = new int[N];
+        sel = new int[R];
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for(int i = 0; i < N; i++) {
+        	input[i] = i+1;
+        }
+        combi(0,0);
 
-		String[] line = br.readLine().split(" ");
-		N = Integer.parseInt(line[0]);
-		M = Integer.parseInt(line[1]);
-
-		isChecked = new boolean[N];
-		numbers = new int[M];
-
-		combination(0, 1);
-
-	}
-
-	/*
-	 * index: 1~M까지 start: 시작하는 숫자
-	 */
-	private static void combination(int index, int start) {
-
-		if (index == M) {
-			StringBuilder sb = new StringBuilder();
-			for(int num : numbers)
-				sb.append(num + " ");
-			System.out.println(sb);
-			return;
-		}
-
-		for (int i = start; i <= N; i++) {
-			numbers[index] = i;
-			combination(index + 1, i + 1);
-		}
-	}
+    }
+    static void combi(int cnt, int start) {
+        if(cnt == R) {
+            for(int i=0; i<R; i++) {
+            	System.out.print(sel[i] +" ");
+            	if((i+1)%R == 0)
+            		System.out.println();
+            }
+            return;
+        }
+        
+        for(int i = start ; i <  N; i++) {
+            sel[cnt]  = input[i];
+            combi(cnt + 1, i+1);
+        }
+    }
 }
