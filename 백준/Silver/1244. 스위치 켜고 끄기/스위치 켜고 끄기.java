b8aed8947fbc502effaf9d576459d2af55d16120
wonju <101818687+3o14@users.noreply.github.com>
2023-08-02 09:53:30 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 144 ms, Memory: 14492 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,77 +1,74 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int start, end = 1;
-
+	
+	public static int[] arr;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int switchNum = Integer.parseInt(br.readLine()); // 스위치 개수
-		String[] tmp = br.readLine().split(" ");
-
-		// 입력
-		int[] status = new int[switchNum + 1];
-		for (int i = 0; i < tmp.length; i++) {
-			status[i+1] = Integer.parseInt(tmp[i]);
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		arr = new int[n];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-
-		// 학생 정보 입력
-		int studentNum = Integer.parseInt(br.readLine());
-		while (studentNum-- > 0) {
-			String[] line = br.readLine().split(" ");
-			int gender = Integer.parseInt(line[0]);
-			int num = Integer.parseInt(line[1]);
-
-			if (gender == 1) { // 남학생
-				for (int j = 1; j <= switchNum / num; j++) {
-					if (status[num * j] == 1) {
-						status[num * j] = 0;
-					} else {
-						status[num * j] = 1;
-					}
-				}
-			} else if (gender == 2) { // 여학생
-				changeStatus(num, status, 1);
-
-				if (end != 1) {
-					for (int j = start; j <= end; j++) { // 가장 많은 대칭 범위를 상태변경?
-						if (status[j] == 1) {
-							status[j] = 0;
-						} else {
-							status[j] = 1;
-						}
-					}
-					start = 1;
-					end = 1;
-				}
-				else {
-					if (status[num] == 1) {
-						status[num] = 0;
-					} else {
-						status[num] = 1;
-					}
-				}
+		
+		int m = Integer.parseInt(br.readLine());
+		
+		int []data = new int[2];
+		for(int i=0; i<m; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<2; j++) {
+				data[j] = Integer.parseInt(st.nextToken());
+			}
+			
+			if(data[0] == 1) { // 남자일 경우
+				arr = boy(data[1]);
+			} else { // 여자일 경우
+				//
+				arr = girl(data[1]);
 			}
 		}
-		// 출력
-		for (int i = 1; i <= switchNum; i++) {
-			System.out.print(status[i] + " ");
-			if (i % 20 == 0)
+		int cnt = 1;
+		
+//		int[] arr2 = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,};
+		for(int i=0; i<arr.length; i++) {
+			if(i == 20*cnt) {
 				System.out.println();
+				cnt++;
+			}
+			System.out.print(arr[i] +" ");
 		}
 	}
-
-	public static void changeStatus(int index, int[] status, int i) {
-		if (index - i >= 1 && index + i < status.length) {
-			if (status[index - i] == status[index + i]) { // 대칭이면
-				start = index - i;
-				end = index + i;
-				changeStatus(index, status, i + 1);
+	
+	public static int[] boy(int data) {
+		for (int i = 0; i < arr.length; i++) {
+			if ((i + 1) % data == 0) {
+				arr[i] = arr[i] == 0 ? 1 : 0;
 			}
-			return;
 		}
-		return;
+		return arr;
+	}
+	
+	public static int[] girl(int data) {
+		data--;
+		arr[data] = arr[data] == 1 ? 0 : 1;
+		for(int i=1; i<=data; i++) {
+			if(data+i >= arr.length || data-i < 0) {
+				break;
+			}
+			if(arr[data+i] == arr[data-i]) {
+				arr[data+i] = arr[data+i] == 1 ? 0 : 1;
+				arr[data-i] = arr[data-i] == 1 ? 0 : 1;
+			} else {
+				return arr;
+			}
+		}
+		return arr;
 	}
 }
