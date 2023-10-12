61e80b6a30433dbd67311d9a407b9f785a829616
wonju <101818687+3o14@users.noreply.github.com>
2023-10-12 16:13:40 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벌꿀채취, Time: 123 ms, Memory: 21,424 KB -BaekjoonHub

--- SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
+++ SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
@@ -3,89 +3,82 @@ import java.util.*;
 
 class Solution {
 
-static int N, M, C, res, map[][], profit[][];
-    
-    public static void main(String[] args) throws IOException {
-        
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        
-        int T = Integer.parseInt(br.readLine());
-        for (int tc = 1; tc <= T; tc++) {
-            
-            st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken()); // N : 벌통의 크기
-            M = Integer.parseInt(st.nextToken()); // M : 채취할 수 있는 벌통의 수
-            C = Integer.parseInt(st.nextToken()); // C : 두 일꾼이 채취할 수 있는 꿀의 최대 양
-            profit = new int[N][N]; // 꿀을 담을 용기
-            map = new int[N][N];
-            
-            for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine());
-                for (int j = 0; j <N; j++) {
-                    map[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
-            
-            res = 0;
-            process();
-            
-            System.out.println("#" + tc + " " + res);
-        }
- 
-    }
- 
-    private static void process() {
-        
-        // 꿀을 채취할 수 있는 구간에서 얻을 수 있는 최대 수익
-        makeProfit();
-        // 일꾼 A가 채취할 구간
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j <= N - M; j++) {
-                combination(i, j + M, 1, profit[i][j]);
-            }
-        }
-        
-    }
- 
-    private static void combination(int x, int y, int cnt, int sum) {
-        
-        if(cnt == 2) {
-            res = Math.max(res, sum);
-            return;
-        }
-        // 일꾼 B가 채취할 구간
-        for (int i = x; i < N; i++) {
-            for (int j = y; j <= N - M; j++) {
-                combination(i, j, cnt + 1, sum + profit[i][j]);
-            }
-            y = 0;
-        }
-    }
- 
-    private static void makeProfit() {
-        
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j <= N - M; j++) {
-                // 여기서 얻을 수 있는 최대 수익(부분집합)
-                profitSubset(i, j, 0, 0, 0);
-            }
-        }
-        
-    }
- 
-    private static void profitSubset(int i, int j, int cnt, int sum, int totalSum) {
-        
-        if(sum > C) return;
-        if(cnt == M) {
-            // 해당 구간에서 최대 수익 갱신
-            profit[i][j - M] = Math.max(profit[i][j - M], totalSum);
-            return;
-        }
-        // 이 꿀을 채취해보자
-        profitSubset(i, j + 1, cnt + 1, sum + map[i][j], totalSum + map[i][j] * map[i][j]);
-        // 이 꿀은 채취 안하고
-        profitSubset(i, j + 1, cnt + 1, sum, totalSum);
-        
-    }
+	static int N, M, C, ans;
+	static int map[][], honey[][];
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int tc = 1; tc <= T; tc++) {
+			sb.append("#").append(tc).append(" ");
+			
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			C = Integer.parseInt(st.nextToken());
+			
+			map = new int[N][N];
+			honey = new int[N][N];
+			
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			// input end
+			
+			ans = 0;
+			solving();
+			
+			sb.append(ans).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+	
+	private static void solving() {
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j <= N-M; j++) {
+				makeSet(i, j, 0, 0, 0);
+			}
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j <= N-M; j++) {
+				combi(i, j+M, 1, honey[i][j]);
+			}
+		}
+		
+	}
+	
+	private static void combi(int i, int j, int cnt, int max) {
+		if(cnt == 2) {
+			ans = Math.max(max, ans);
+			return;
+		}
+		
+		for (int k = i; k < N; k++) {
+			for (int k2 = j; k2 <= N-M; k2++) {
+				combi(k, k2, cnt+1, max+honey[k][k2]);
+			}
+			j = 0;
+		}
+	}
+
+	private static void makeSet(int i, int j, int cnt, int sum, int total) {
+		if(sum > C) return;
+		if(cnt == M) {
+			honey[i][j-M] = Math.max(honey[i][j-M], total);
+			return;
+		}
+		
+		makeSet(i, j+1, cnt+1, sum+map[i][j], total+map[i][j]*map[i][j]);
+		makeSet(i, j+1, cnt+1, sum, total);
+	}
+
 }
