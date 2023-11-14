6198f8cdbb0d5d9253e6dd3e7db87e0720aea353
wonju <101818687+3o14@users.noreply.github.com>
2023-11-14 17:15:15 +0900

    [D3] Title: 재미있는 오셀로 게임, Time: 110 ms, Memory: 18,388 KB -BaekjoonHub

---
+++ SWEA/D3/4615. 재미있는 오셀로 게임/재미있는 오셀로 게임.java
@@ -0,0 +1,113 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+	static int T, N, M, ans, blackCnt, whiteCnt;
+	static int map[][];
+	static final int BLACK = 1, WHITE = 2;
+	static int dr[] = { -1, -1, 0, 1, 1, 1, 0, -1 }, dc[] = { 0, 1, 1, 1, 0, -1, -1, -1 };
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+			sb.append("#").append(tc).append(" ");
+
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+
+			map = new int[N + 1][N + 1];
+
+			map[N / 2][N / 2] = WHITE;
+			map[N / 2 + 1][N / 2] = BLACK;
+			map[N / 2][N / 2 + 1] = BLACK;
+			map[N / 2 + 1][N / 2 + 1] = WHITE;
+
+			for (int i = 0; i < M; i++) {
+				if (isEnd())
+					break;
+
+				st = new StringTokenizer(br.readLine());
+				int r = Integer.parseInt(st.nextToken());
+				int c = Integer.parseInt(st.nextToken());
+				int color = Integer.parseInt(st.nextToken());
+
+				map[r][c] = color;
+				change(r, c, color);
+
+			}
+
+			blackCnt = 0;
+			whiteCnt = 0;
+			count();
+
+			sb.append(blackCnt).append(" ").append(whiteCnt).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+	private static boolean isEnd() {
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				if (map[i][j] == 0) {
+					return false;
+				}
+
+			}
+		}
+		return true;
+	}
+
+	private static void count() {
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				if (map[i][j] == BLACK) {
+					blackCnt++;
+				} else if (map[i][j] == WHITE) {
+					whiteCnt++;
+				}
+
+			}
+		}
+
+	}
+
+	private static void change(int r, int c, int color) {
+		int atomy = color == BLACK ? WHITE : BLACK;
+		
+		for (int i = 0; i < dc.length; i++) {
+			int nr = r;
+			int nc = c;
+
+			boolean thereIs = false;
+			while (true) {
+				nr += dr[i];
+				nc += dc[i];
+
+				if (nr > N || nr < 0 || nc > N || nc < 0)
+					break;
+				if(map[nr][nc] == 0) break;
+				if (map[nr][nc] == color) {
+					thereIs = true;
+					break;
+				}
+			}
+			while(thereIs) {
+				if(nr == r && nc == c) break;
+				if(map[nr][nc] == atomy) {
+					map[nr][nc] = color;
+				}
+				nr -= dr[i];
+				nc -= dc[i];
+			}
+
+		}
+	}
+
+}
