dddb429121fd784f8e3ea4ad9e589124837e5fc2
Lee Wonju <101818687+3o14@users.noreply.github.com>
2023-12-17 14:49:06 +0900

    [level 1] Title: 달리기 경주, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -1,19 +1,27 @@
+// 1. 이름: 인덱스 형식의 객체를 만든다.
+// 2. 이름이 불린 값의 value-1의 value를 가진 값을 +1 해준다.
+// 3. 이름이 불린 값의 value를 -1 해준다.
+// 4. 위와 같은 방식으로 callings 배열 순회가 끝났다면 result 배열에 value값 순서대로 sorting하고 출력
 function solution(players, callings) {
-    const hash = new Map();
+    const obj = {};
     
     for(let i=0; i<players.length; i++) {
-        hash.set(players[i], i);
+        obj[players[i]] = i;
     }
     
+    let prev, cur, name, idx;
     for(let i=0; i<callings.length; i++) {
-        const name = callings[i];
-        const curIdx = hash.get(name);
-        const front = players[curIdx-1];
+        name = callings[i];
+        idx = obj[name];
         
-        [players[curIdx], players[curIdx-1]] = [front, players[curIdx]];
+        prev = players[idx-1];
+        cur = players[idx];
         
-        hash.set(name, hash.get(name)-1);
-        hash.set(front, hash.get(name)+1);
+        obj[cur] -= 1;
+        obj[prev] += 1;
+        
+        players[idx-1] = cur;
+        players[idx] = prev;
     }
     
     return players;
