6f74a167353682012f75c234c666ff4aa35f94c3
Lee Wonju <101818687+3o14@users.noreply.github.com>
2023-12-17 14:35:31 +0900

    [level 1] Title: 달리기 경주, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -1,7 +1,3 @@
-// 1. 이름: 인덱스 형식의 객체를 만든다.
-// 2. 이름이 불린 값의 value-1의 value를 가진 값을 +1 해준다.
-// 3. 이름이 불린 값의 value를 -1 해준다.
-// 4. 위와 같은 방식으로 callings 배열 순회가 끝났다면 result 배열에 value값 순서대로 sorting하고 출력
 function solution(players, callings) {
     const hash = new Map();
     
