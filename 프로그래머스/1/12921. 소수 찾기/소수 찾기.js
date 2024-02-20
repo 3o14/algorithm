7f1a9360c134609bd093a1cab261c29991e0d312
Lee Wonju <101818687+3o14@users.noreply.github.com>
2024-02-20 16:14:01 +0900

    [level 1] Title: 소수 찾기, Time: 37.34 ms, Memory: 43.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,12 @@
+function solution(n, ans=0) {
+    let primes = new Array(n+1).fill(true)
+    for(let i=2; i<=Math.sqrt(n) ;i++) {
+        for(let j=i*i; j<=n; j+=i) {
+            primes[j] = false
+        }
+    }
+    primes.forEach((e, i) => {
+        if(i>=2&&e) ans++
+    })
+    return ans
+}
