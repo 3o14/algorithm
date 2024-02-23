1dc10a454d1d659d1a02b1886fc1e4e39001036b
Lee Wonju <101818687+3o14@users.noreply.github.com>
2024-02-23 13:56:43 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,37 +1,5 @@
-const win = (n) => {
-    switch(n) {
-        case 6:
-            return 1
-        case 5:
-            return 2
-        case 4:
-            return 3
-        case 3:
-            return 4
-        case 2:
-            return 5
-        case 1:
-            return 6
-        default:
-            return 6
-    }
-}
-
 function solution(lottos, win_nums) {
-    let zero = 0
-    let sameNum = 0
-    for(let i=0; i<lottos.length; i++) {
-        if(lottos[i] === 0) {
-            zero += 1
-            continue
-        }  
-        for(let j=0; j<win_nums.length; j++) {
-            if(lottos[i] === win_nums[j]) {
-                sameNum += 1
-            }
-            
-        }
-    }
-    
-    return [win(sameNum), win(sameNum+zero)].sort((a, b) => a-b)
+    const min = lottos.filter((e) => win_nums.includes(e)).length
+    const max = lottos.filter((e) => e === 0).length
+    return [!(min+max) ? 6 : 7-(min+max), !min ? 6 : 7-min ]
 }
