590cfef9e7dd116c62695bd0ebe67219ba4ebf18
Lee Wonju <101818687+3o14@users.noreply.github.com>
2024-02-26 11:06:47 +0900

    [level 1] Title: 대충 만든 자판, Time: 11.42 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -1,38 +1,41 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
 function solution(keymap, targets) {
-    let ans = new Array(targets.length).fill(0)
-    let arr = new Array(26).fill(0) // 65~90
-    // console.log(String.fromCodePoint(90))
-    // console.log('A'.codePointAt(0))
-    let target = null
-    for(let i=0; i<targets.length; i++) {
-        target = [...targets[i]] // ["A", "B", "C", ...]
-        
-        for(let j=0; j<target.length; j++) {
-            const word = target[j] // "A"
-            // 알파벳 배열에 최소 횟수 정보가 담겨 있으면 그걸 쓰고
-            if(arr[word.codePointAt(0)-65]) {
-                ans[i] += arr[word.codePointAt(0)-65]
-                continue
-            }
-            
-            // 없으면 최소 횟수 구하기
-            let minIdx = Number.MAX_VALUE
-            for(let k=0; k<keymap.length; k++) {
-                const idx = keymap[k].indexOf(word)
-                minIdx = Math.min(idx === -1 ? minIdx : idx, minIdx)
-            }
-            
-            // 없을 경우 -1 넣고 다음 target으로 넘어감
-            if(minIdx === Number.MAX_VALUE) {
-                ans[i] = -1
-                break
-            }
-            
-            // 구한 값 배열에 저장하기
-            arr[word.codePointAt(0)-65] = minIdx+1
-            ans[i] += minIdx+1
-        }
-    }
-    
-    return ans
+    let answer = Array.from({length : targets.length}, ()=> 0);
+
+    targets.forEach((target, index)=>{
+       for(let i = 0; i<target.length; i++){
+           let min = Number.MAX_SAFE_INTEGER;
+           keymap.forEach((kv, ki) => {
+               let k = kv.indexOf(target[i]);
+               if(k !== -1) min = Math.min(k + 1, min);
+           })
+           if(min === Number.MAX_SAFE_INTEGER) {
+               answer[index] = -1;
+               break;
+           }
+           else answer[index] += min; 
+       }
+    })
+
+    return answer;
 }
