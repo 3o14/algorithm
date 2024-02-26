848557634e722e32d1f13d649395481308150d14
Lee Wonju <101818687+3o14@users.noreply.github.com>
2024-02-26 11:01:01 +0900

    [level 1] Title: 대충 만든 자판, Time: 1.98 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -0,0 +1,38 @@
+function solution(keymap, targets) {
+    let ans = new Array(targets.length).fill(0)
+    let arr = new Array(26).fill(0) // 65~90
+    // console.log(String.fromCodePoint(90))
+    // console.log('A'.codePointAt(0))
+    let target = null
+    for(let i=0; i<targets.length; i++) {
+        target = [...targets[i]] // ["A", "B", "C", ...]
+        
+        for(let j=0; j<target.length; j++) {
+            const word = target[j] // "A"
+            // 알파벳 배열에 최소 횟수 정보가 담겨 있으면 그걸 쓰고
+            if(arr[word.codePointAt(0)-65]) {
+                ans[i] += arr[word.codePointAt(0)-65]
+                continue
+            }
+            
+            // 없으면 최소 횟수 구하기
+            let minIdx = Number.MAX_VALUE
+            for(let k=0; k<keymap.length; k++) {
+                const idx = keymap[k].indexOf(word)
+                minIdx = Math.min(idx === -1 ? minIdx : idx, minIdx)
+            }
+            
+            // 없을 경우 -1 넣고 다음 target으로 넘어감
+            if(minIdx === Number.MAX_VALUE) {
+                ans[i] = -1
+                break
+            }
+            
+            // 구한 값 배열에 저장하기
+            arr[word.codePointAt(0)-65] = minIdx+1
+            ans[i] += minIdx+1
+        }
+    }
+    
+    return ans
+}
