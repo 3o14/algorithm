896d59252755fc111f7bba24550545c87fb97474
Lee Wonju <101818687+3o14@users.noreply.github.com>
2024-02-26 16:23:21 +0900

    [level 1] Title: 둘만의 암호, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -1,14 +1,20 @@
 function solution(s, skip, index, ans='') {
     let alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    // 중간 문자열을 자를때 성능면에서 어떤게 더 낫지?
+    
+    // 1. skip할 문자를 replace로 자른다
     skip = [...skip].forEach(e => {
-        alphabet = alphabet.replace(e, '')
+        alphabet = alphabet.replace(e, '') // 중간 문자열을 자를때 성능면에서 어떤게 더 낫지?
     })
+    
+    // 2. skip이 제거된 알파벳 문자열을 배열로 만든다.
     alphabet = [...alphabet]
+    
+    // 3. s를 돌면서 index를 더한 값을 ans에 더한다.
     s = [...s].forEach(e => {
-        let idx = (alphabet.indexOf(e) + index) % alphabet.length
+        let idx = (alphabet.indexOf(e) + index) % alphabet.length // 3.1 z를 넘었을 경우 a로 돌아오기
         ans += alphabet[idx]
     })
     
+    // 4. 정답 반환
     return ans
 }
