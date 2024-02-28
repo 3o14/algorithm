2193dede03c9cce7d7835900b3a040a0ec763944
Lee Wonju <101818687+3o14@users.noreply.github.com>
2024-02-28 12:34:33 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 4.09 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -1,55 +1,47 @@
-// info Map를 생성한다. (key: 친구이름, value: 친구의 인덱스)
-// -1로 채운 friends 길이의 2차원 배열 map을 만든다.
-// gifts를 돌면서 선물을 주고받은 정보를 map에 넣는다. (이때 info 객체 이용)
-// 이제 map을 돌면서 준 선물([i][j]), 받은 선물([j][i])의 개수를 확인하고 이름 배열에 담는다.
 function solution(friends, gifts) {
-    let info = new Map()
-    let names = new Array(friends.length).fill(0)
-    let gPercent = new Array(friends.length).fill(0)
+    let info = new Map()    // 친구 이름으로 인덱스 번호를 얻기 위한 Map (key: 이름, value: 인덱스)
+    let names = new Array(friends.length).fill(0)   // 다음 달에 받을 선물 개수를 담을 배열
+    let gPercent = new Array(friends.length).fill(0)    // 선물 지수를 저장할 배열
+    let map = Array.from(new Array(friends.length), () => new Array(friends.length).fill(0))
     
+    // info Map에 정보 저장
     friends.forEach((e, i) => {
         info.set(e, i)
     })
     
-    let map = Array.from(new Array(friends.length), () => new Array(friends.length).fill(0))
-    
+    // 선물 주고받은 정보를 토대로 map에 저장과 동시에 선물 지수 측정
     gifts.forEach((e) => {
         let [giveOne, getOne] = e.split(' ')
         map[info.get(giveOne)][info.get(getOne)] += 1    
+        gPercent[info.get(giveOne)] += 1
+        gPercent[info.get(getOne)] -= 1
     })
-     
-    // 선물지수 구하기 -> 더 깔끔하게 하고싶다
-    for(let i=0; i<friends.length; i++) {
-        let giveSum = 0
-        for(let j=0; j<friends.length; j++) {
-            giveSum += map[i][j]   
-        }
-        gPercent[i] = giveSum
-    }
-    
-    for(let j=0; j<friends.length; j++) {
-        let giveSum = 0
-        for(let i=0; i<friends.length; i++) {
-            giveSum += map[i][j]   
-        }
-        gPercent[j] -= giveSum
-    }
-
+      
+    // 누가 더 많이 줬는지 names에 저장
     for(let i=0; i<friends.length; i++) {
         for(let j=0; j<friends.length; j++) {
+            
+            // 자기자신이거나 이미 확인한 경우 넘어감
             if(i === j) continue
             if(map[i][j] === -1) continue
+            
+            // 누가 더 많이 줬는지 result에 담기
             const result = map[i][j] - map[j][i]
+            
+            // 방문 체크
             map[i][j] = -1
             map[j][i] = -1
+            
+            // 더 많이 준 사람을 names 배열에 1 더함
             if(result > 0) names[i] += 1
             if(result < 0) names[j] += 1
-            if(result === 0) {
-                if(gPercent[i] === gPercent[j]) continue
+            if(result === 0) { // 같을 경우 선물 지수에 따라 결정
+                if(gPercent[i] === gPercent[j]) continue // 선물 지수도 같으면 그냥 넘어감
                 gPercent[i] > gPercent[j] ? names[i] += 1 : names[j] += 1
             }
         }
     }
     
+    // names 중 가장 큰 값 반환
     return Math.max(...names)
 }
