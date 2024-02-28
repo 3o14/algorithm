65c78497989ebd70bbc40453fa1be70292f024a9
Lee Wonju <101818687+3o14@users.noreply.github.com>
2024-02-28 16:37:19 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,14 @@
+function solution(n, ans=0) {
+    while(n > 0) {
+        if(!n) break
+        
+        if(n%2 === 0) {
+          n /= 2
+        } else {
+            ans += 1
+            n -= 1
+        }
+    }
+    
+    return ans
+}
