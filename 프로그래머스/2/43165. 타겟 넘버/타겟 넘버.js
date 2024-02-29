aaa0d29a2f1173f34a81baa61320aaead3654331
Lee Wonju <101818687+3o14@users.noreply.github.com>
2024-02-29 11:07:22 +0900

    [level 2] Title: 타겟 넘버, Time: 5.23 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -10,6 +10,7 @@ function solution(numbers, target, ans=0) {
                 sum += visit[i] ? numbers[i] : -numbers[i]   // visit[i]가 true면 더하기 false면 빼기
             })
             if(sum === target) ans += 1    // sum이 target과 같으면 ans에 1 더하기
+            return
         } else {
             visit[depth] = true
             dfs(depth+1)
