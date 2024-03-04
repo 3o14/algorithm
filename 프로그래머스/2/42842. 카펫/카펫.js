b0ab96bc538b7e7addbe7811adf9a1f55d65bd4e
Lee Wonju <101818687+3o14@users.noreply.github.com>
2024-03-04 10:45:44 +0900

    [level 2] Title: 카펫, Time: 0.22 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,14 +1,7 @@
-// 1. 각 모서리를 빼고 (-4)
-// 2. 윗변과 왼쪽변의 길이합을 구해서 (/2)
-// 3. 합이 위의값이 나오는 길이 2의 조합을 구한다. (예: 값이 3이면 [1, 2], [2, 1])
-// 4. 나온 조합을 전부 탐색해보고 yellow가 개수에 맞게 들어가는지 확인한다.
-
-// yellow의 경우의 수 구하기
-// 1. yellow/n이 0으로 나누어 떨어지는 수를 x라고 하자
-// 2. 1부터 yellow까지 x의 몫을 곱한다.
-// 3. 그 곱 조합이 yellow 부분의 크기가 된다.
-// 4. 이 크기가 brown의 조합과 맞는게 있다면 각 가로 세로에 2씩 더해서 반환한다.
 function solution(brown, yellow) {
+    // 1. 각 모서리를 빼고 (-4)
+    // 2. 윗변과 왼쪽변의 길이합을 구해서 (/2)
+    // 3. 합이 위의값이 나오는 길이 2의 조합을 구한다. (예: 값이 3이면 [1, 2], [2, 1])
      const num = (brown-4)/2
      let set = []
      
@@ -17,21 +10,21 @@ function solution(brown, yellow) {
          set.push([i, num-i])
          i += 1
      }
+    // 4. 나온 조합을 전부 탐색해보고 yellow가 개수에 맞게 들어가는지 확인한다.
     
+    // yellow의 경우의 수 구해서 set의 원소와 같은 것을 찾는다.
     let j=1
-    if(yellow === 1) return [3, 3]
+    if(yellow === 1) return [3, 3] // yellow가 1인 경우는 3*3 한 가지 밖에 나올 수 없다.
     while(j < yellow) {
+        // yellow가 될 수 있는 가로 세로 곱 조합을 구한다.
         if(yellow%j === 0) {
-            console.log([j, yellow/j])
             for(const e of set) {
-                // console.log(e, [j, yellow/j], e.toString() === [j, yellow/j].toString())
-                if(e.toString() === [j, yellow/j].toString()) {
+                if(e.toString() === [j, yellow/j].toString()) { // 같은 것을 찾을 경우
                     let arr = [j, yellow/j]
-                    arr = arr.sort((a, b) => b-a)
-                    return [arr[0]+2, arr[1]+2]
+                    arr = arr.sort((a, b) => b-a) // 가로가 무조건 더 길게
+                    return [arr[0]+2, arr[1]+2] // brown의 경우와 yellow의 경우가 같으면 값 반환
                 }
             }
-            // set.indexOf[j, yellow/j]
         }
         j += 1
     }
