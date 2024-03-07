ed67dca72edb1a4c1d7947e8a54c46b8ea849967
Lee Wonju <101818687+3o14@users.noreply.github.com>
2024-03-07 10:09:01 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,8 +1,9 @@
 function solution(ingredient, ans=0) {
     for(let i=0; i<ingredient.length; i++) {
+        // i부터 i+4번째가 햄버거이면 그 부분을 잘라내고 인덱스를 3만큼 되돌려주고 정답 개수를 1 더한다
         if(ingredient.slice(i, +i+4).toString() === [1, 2, 3, 1].toString()) {
             ingredient.splice(i, 4)
-            i -= 4
+            i -= 3
             ans += 1
         }
     }
