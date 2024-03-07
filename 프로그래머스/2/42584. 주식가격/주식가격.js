44cb49431789e094ae649ab9e5b3c0d0d76c22f4
Lee Wonju <101818687+3o14@users.noreply.github.com>
2024-03-07 10:38:02 +0900

    [level 2] Title: 주식가격, Time: 6.01 ms, Memory: 42.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,12 +1,15 @@
 function solution(prices, ans=[]) {
     prices.forEach((e, i) => {
-        let min = 1
+        let min = 1 // 항상 1초간은 가격 안떨어지기 때문에 1로 시작
+        
+        // 현재 주식 가격부터 가격 떨어지는지 확인
         for(let j=i+1; j<prices.length-1; j++) {
+            // 가격이 떨어지지 않으면
             if(e <= prices[j]) {
                 min += 1
-            } else break
+            } else break // 떨어지면 그만 보기
         }
-        ans.push(i === prices.length-1 ? 0 : min)
+        ans.push(i === prices.length-1 ? 0 : min) // 마지막 요소이면 0, 아니면 min 반환
     })
     
     return ans
