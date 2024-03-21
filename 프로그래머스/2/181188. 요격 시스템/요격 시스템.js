3603590ce130a6ac46d33497c63ee207458b510d
이원주 <101818687+3o14@users.noreply.github.com>
2024-03-21 12:44:01 +0900

    [level 2] Title: 요격 시스템, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
@@ -1,16 +1,4 @@
-function solution(targets, ans=1) {
-    targets.sort((a, b) => b[0] - a[0])
-    
-    let point = targets[0][0]
-    
-    for(let i=1; i<targets.length; i++) {
-        const [start, end] = targets[i]
-        
-        if(end <= point) {
-            ans += 1
-            point = start
-        }
-    }
-    
-    return ans
+function solution(targets) {
+    var answer = 0;
+    return answer;
 }
