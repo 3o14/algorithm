fdab9351a9aad77f6f56276fa8d37b8be76dc8f6
이원주 <101818687+3o14@users.noreply.github.com>
2024-03-27 10:09:16 +0900

    [level 2] Title: 방문 길이, Time: 0.63 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -1,19 +1,33 @@
-function solution(dirs) {
-    let move = { L: [-1, 0], R: [1, 0], U: [0, 1], D: [0, -1] };
-    let now = [0, 0];
-    let route = new Set();
+/*
+    문제 요약: 이미 갔던 길을 제외하고 총 이동한 거리 출력하기
     
-    for (let dir of dirs) {
-        let nowX = now[0] + move[dir][0];
-        let nowY = now[1] + move[dir][1];
-        
-        if (nowX > 5 || nowX < -5 || nowY > 5 || nowY < -5) continue;
-        
-        route.add("" + now[0] + now[1] + nowX + nowY);
-        route.add("" + nowX + nowY + now[0] + now[1]);
-        
-        now = [nowX, nowY];
+    set에 이동한 위치를 (시작점-출발점) 형식으로 넣는다. (set -> 중복 제거)
+    
+*/
+
+function solution(dirs) {
+    let set = new Set()
+    let now = [0, 0]
+    
+    const move = {
+        D: [1, 0],
+        U: [-1, 0],
+        R: [0, 1],
+        L: [0, -1],
     }
     
-    return route.size / 2;
+    for(let dir of dirs) {
+        let nr = now[0] + move[dir][0]
+        let nc = now[1] + move[dir][1]
+        
+        if(nr < -5 || nr > 5 || nc < -5 || nc > 5) continue
+        
+        // 시작점-출발점 을 함께 Set하기
+        set.add(`${now[0]}${now[1]}${nr}${nc}`)
+        set.add(`${nr}${nc}${now[0]}${now[1]}`)
+
+        now = [nr, nc]
+    }
+    
+    return set.size/2
 }
