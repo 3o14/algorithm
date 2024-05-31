f03e33dcbd5fce44a3181c3aa0a9de8f5608119b
이원주 <101818687+3o14@users.noreply.github.com>
2024-05-31 18:30:16 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 148 ms, Memory: 15292 KB -BaekjoonHub

--- 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.java
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.java
@@ -4,6 +4,14 @@ import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
+/*
+    카드 합체 놀이
+    문제 요약: 카드 합체를 k번 한 후 카드에 적힌 수의 합을 최소로 만들기
+    
+    1. 우선순위 큐를 사용해서 가장 적은 카드 두 장을 뽑아 더한 후 덮어쓰기 해준다.
+    2. 마지막에 모든 카드의 숫자를 합한 값을 출력한다.
+*/
+
 public class Main {
 
     public static void main(String[] args) throws IOException {
@@ -16,11 +24,11 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) pq.add(Long.parseLong(st.nextToken()));
 
-        for (int i = 0; i < m; i++) { // 시간 복잡도 : O(4 * m)
+        for (int i = 0; i < m; i++) {
             long a = pq.poll();
             long b = pq.poll();
 
-            long sum = a + b; /** a a a ... -> a a ... 2a 2a -> int 범위 넘어가버림 **/
+            long sum = a + b;
             pq.add(sum);
             pq.add(sum);
         }
